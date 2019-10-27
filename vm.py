bc0538f522e791a462d3edbab3273ebba9c2c5fa
Andrew Kay <kaya3@aston.ac.uk>
2019-10-27 04:51:17 +0000

    stack-based arithmetic

--- vm.py
+++ vm.py
@@ -8,15 +8,13 @@ IO_REGISTER = 2
 MAX_INT = 2**16 - 1
 
 class State:
-	def __init__(self, registers=None, top_of_stack=0, stack=None):
+	def __init__(self, registers=None, stack=None):
 		self.registers = (0,)*REGISTER_COUNT if registers is None else registers
-		self.top_of_stack = top_of_stack
 		self.stack = [] if stack is None else stack
 	
 	def __repr__(self):
-		return 'State(registers={!r}, top_of_stack={!r}, stack={!r})'.format(
+		return 'State(registers={!r}, stack={!r})'.format(
 			self.registers,
-			self.top_of_stack,
 			self.stack
 		)
 	
@@ -51,17 +49,14 @@ class VM:
 		registers = list(self.state.registers)
 		registers[PC_REGISTER] += 1
 		
-		top_of_stack = self.state.top_of_stack
 		stack = list(self.state.stack)
 		
 		if op == 'CONST':
-			registers[arg1] = arg2
+			stack.push(arg1)
 		elif op == 'PUSH':
 			stack.append(registers[arg1])
-			top_of_stack += 1
 		elif op == 'POP':
 			registers[arg1] = stack.pop()
-			top_of_stack -= 1
 		elif op == 'LOAD':
 			addr = self.state.base_of_stack + registers[arg2]
 			registers[arg1] = stack[addr]
@@ -70,39 +65,41 @@ class VM:
 			stack[addr] = registers[arg1]
 		elif op == 'COPY':
 			registers[arg1] = registers[arg2]
-		elif op == 'INPUT':
-			x = registers[arg1]
-			y = registers[arg2]
-			registers[IO_REGISTER] = self.input_grid[y][x]
-		elif op == 'OUTPUT':
-			x = registers[arg1]
-			y = registers[arg2]
-			self.output_grid[y][x] = registers[IO_REGISTER]
 		elif op == 'JUMPIF':
-			if registers[arg2] == 0:
+			if stack.pop() == 0:
 				registers[PC_REGISTER] = registers[arg1]
-		elif op == 'ADD':
-			registers[arg1] = (registers[arg1] + registers[arg2]) & MAX_INT
-		elif op == 'MULT':
-			registers[arg1] = (registers[arg1] * registers[arg2]) & MAX_INT
-		elif op == 'AND':
-			registers[arg1] = registers[arg1] & registers[arg2]
-		elif op == 'OR':
-			registers[arg1] = registers[arg1] | registers[arg2]
-		elif op == 'LSHIFT':
-			registers[arg1] = (registers[arg1] << registers[arg2]) & MAX_INT
-		elif op == 'RSHIFT':
-			registers[arg1] = registers[arg1] >> registers[arg2]
-		elif op == 'EQUALS':
-			registers[arg1] = int(registers[arg1] == registers[arg2])
-		elif op == 'LESSTHAN':
-			registers[arg1] = int(registers[arg1] < registers[arg2])
 		elif op == 'NOT':
-			registers[arg1] = (~registers[arg1]) & MAX_INT
+			x = stack.pop()
+			stack.push(~x & MAX_INT)
 		else:
-			raise ValueError('Unknown op: ' + op)
+			y = stack.pop()
+			x = stack.pop()
+			if op == 'INPUT':
+				registers[IO_REGISTER] = self.input_grid[y][x]
+			elif op == 'OUTPUT':
+				self.output_grid[y][x] = registers[IO_REGISTER]
+			elif op == 'ADD':
+				stack.push((x + y) & MAX_INT)
+			elif op == 'MULT':
+				stack.push((x * y) & MAX_INT)
+			elif op == 'AND':
+				stack.push(x & y)
+			elif op == 'OR':
+				stack.push(x | y)
+			elif op == 'XOR':
+				stack.push(x ^ y)
+			elif op == 'LSHIFT':
+				stack.push((x << y) & MAX_INT)
+			elif op == 'RSHIFT':
+				stack.push(x >> y)
+			elif op == 'EQUALS':
+				stack.push(int(x == y))
+			elif op == 'LESSTHAN':
+				stack.push(int(x < y))
+			else:
+				raise ValueError('Unknown op: ' + op)
 		
-		self.state = State(registers, top_of_stack, stack)
+		self.state = State(registers, stack)
 
 def parse_line(line):
 	line = line.split(';')[0].strip()
